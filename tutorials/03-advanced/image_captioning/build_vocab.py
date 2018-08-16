81f4b34de291e9f120f5b696624c2f7bfdd73ccd
Hobson Lane <github@totalgood.com>
2018-08-16 12:23:54 -0700

    add constants.py

--- tutorials/03-advanced/image_captioning/build_vocab.py
+++ tutorials/03-advanced/image_captioning/build_vocab.py
@@ -7,24 +7,26 @@ from pycocotools.coco import COCO
 
 class Vocabulary(object):
     """Simple vocabulary wrapper."""
+
     def __init__(self):
         self.word2idx = {}
         self.idx2word = {}
         self.idx = 0
 
     def add_word(self, word):
-        if not word in self.word2idx:
+        if word not in self.word2idx:
             self.word2idx[word] = self.idx
             self.idx2word[self.idx] = word
             self.idx += 1
 
     def __call__(self, word):
-        if not word in self.word2idx:
+        if word not in self.word2idx:
             return self.word2idx['<unk>']
         return self.word2idx[word]
 
     def __len__(self):
         return len(self.word2idx)
+
 
 def build_vocab(json, threshold):
     """Build a simple vocabulary wrapper."""
@@ -36,8 +38,8 @@ def build_vocab(json, threshold):
         tokens = nltk.tokenize.word_tokenize(caption.lower())
         counter.update(tokens)
 
-        if (i+1) % 1000 == 0:
-            print("[{}/{}] Tokenized the captions.".format(i+1, len(ids)))
+        if (i + 1) % 1000 == 0:
+            print("[{}/{}] Tokenized the captions.".format(i + 1, len(ids)))
 
     # If the word frequency is less than 'threshold', then the word is discarded.
     words = [word for word, cnt in counter.items() if cnt >= threshold]
@@ -54,6 +56,7 @@ def build_vocab(json, threshold):
         vocab.add_word(word)
     return vocab
 
+
 def main(args):
     vocab = build_vocab(json=args.caption_path, threshold=args.threshold)
     vocab_path = args.vocab_path
@@ -66,9 +69,9 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--caption_path', type=str, 
-                        default='data/annotations/captions_train2014.json', 
+                        default='/midata/imdesc/annotations/captions_train2014.json', 
                         help='path for train annotation file')
-    parser.add_argument('--vocab_path', type=str, default='./data/vocab.pkl', 
+    parser.add_argument('--vocab_path', type=str, default='/midata/imdesc/vocab.pkl', 
                         help='path for saving vocabulary wrapper')
     parser.add_argument('--threshold', type=int, default=4, 
                         help='minimum word count threshold')
--- tutorials/03-advanced/image_captioning/resize.py
+++ tutorials/03-advanced/image_captioning/resize.py
@@ -2,10 +2,13 @@ import argparse
 import os
 from PIL import Image
 
+BASE_DIR = '/midata/imdesc'
+
 
 def resize_image(image, size):
     """Resize an image to the given size."""
     return image.resize(size, Image.ANTIALIAS)
+
 
 def resize_images(image_dir, output_dir, size):
     """Resize the images in 'image_dir' and save into 'output_dir'."""
@@ -32,9 +35,9 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image_dir', type=str, default='./data/train2014/',
+    parser.add_argument('--image_dir', type=str, default=BASE_DIR + '/train2014/',
                         help='directory for train images')
-    parser.add_argument('--output_dir', type=str, default='./data/resized2014/',
+    parser.add_argument('--output_dir', type=str, default=BASE_DIR + '/resized2014/',
                         help='directory for saving resized images')
     parser.add_argument('--image_size', type=int, default=256,
                         help='size for image after processing')
